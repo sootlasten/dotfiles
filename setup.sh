a0a3671ba9078b559b06deef43097e6842742fb7
sten <stensootla@gmail.com>
2019-04-10 22:49:05 +0300

    added script that sets up symlinks automatically

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# store where the script was called
+script_path="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# symlink all dotfiles to the home directory
+for f in .vimrc .vim .tmux.conf
+do 
+    ln -s $script_path/$f $HOME/$f
+done
+    
