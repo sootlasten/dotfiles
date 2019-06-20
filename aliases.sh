8a17f729987a630d1de8eaf90ea19635f143de08
Sten Sootla <stensootla@gmail.com>
2019-06-20 15:31:35 +0300

    added alias for killing python processes

--- aliases.sh
+++ aliases.sh
@@ -11,4 +11,10 @@ fi
 echo "# aliases for copying and pasting" >> $bashrc_path
 echo "alias \"c=xclip -selection clipboard\"" >> $bashrc_path
 echo "alias \"p=xclip -o -selection clipboard\"" >> $bashrc_path
+echo "" >> $bashrc_path
+
+# alias for killing all python processes
+echo "# aliases for killing processes" >> $bashrc_path
+echo "alias \"killpy=ps -ef | grep python | awk '{print \\\$2}' | xargs -I{} kill -9 {}\"" >> $bashrc_path
+echo "" >> $bashrc_path
 
